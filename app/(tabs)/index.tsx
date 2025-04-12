0869515ef639495ef485ce0a03f06cf14ebc6489
Leonardo <leonardolujan72@gmail.com>
2025-04-12 14:03:56 -0400

    h

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Weeee!</ThemedText>
+        <ThemedText type="title">Wee!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
